a0f16b323d3b9b6255f99c674e598586dc9222b1
github-cloudlabsuser-2041 <169062455+github-cloudlabsuser-2041@users.noreply.github.com>
2024-11-05 19:56:27 +0000

    copilot third commit

---
+++ terraform.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Location/Region for the storage account"
+  type        = string
+}
